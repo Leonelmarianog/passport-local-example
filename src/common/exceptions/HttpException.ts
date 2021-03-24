fd044587eccc99996653aff25dedd8c4d91d70e5
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-03-24 18:16:33 -0300

    Update HttpException class

--- src/common/exceptions/HttpException.ts
+++ src/common/exceptions/HttpException.ts
@@ -1,8 +1,32 @@
 export class HttpException extends Error {
   constructor(
-    public readonly statusCode: number,
-    public readonly message: string
+    private readonly status: number,
+    private readonly description: string,
+    private readonly details?: Record<string, any>
   ) {
-    super(message);
+    super();
   }
+
+  public getStatus = () => {
+    return this.status;
+  };
+
+  public getDescription = () => {
+    return this.description;
+  };
+
+  public getDetails = () => {
+    return this.details;
+  };
+
+  public createBody = (
+    statusCode: number,
+    message: string,
+    details?: Record<string, any>
+  ) => {
+    if (!details) {
+      return { statusCode, message };
+    }
+    return { statusCode, message, details };
+  };
 }
