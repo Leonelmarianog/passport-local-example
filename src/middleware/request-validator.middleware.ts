c56f1af98e1a21055535968e48b91a44071cf562
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-03-24 19:54:48 -0300

    Update requestValidatorMiddleware

--- src/middleware/request-validator.middleware.ts
+++ src/middleware/request-validator.middleware.ts
@@ -1,18 +1,18 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { validate, ValidationError, ValidatorOptions } from 'class-validator';
 import { HttpException } from '../exceptions/http.exception';
 
 const getConstraints = (errors: ValidationError[]): string[] => {
-  return errors.reduce((constraints: string[], error) => {
+  return errors.reduce((arr: string[], error) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const constraintsArray = Object.values(error.constraints!);
-    return constraints.concat(constraintsArray);
+    const constraints = Object.values(error.constraints!);
+    return arr.concat(constraints);
   }, []);
 };
 
 export const requestValidatorMiddleware = (
   validatorOptions?: ValidatorOptions
-) => {
+): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction) => {
     validate(req.body, validatorOptions).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
