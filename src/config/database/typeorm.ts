1821e8a35daa9355152f8e01dce0fa1836c68854
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-04-01 13:57:40 -0300

    Update database credentials

--- src/config/database/typeorm.ts
+++ src/config/database/typeorm.ts
@@ -1,14 +1,18 @@
-import { ConnectionOptions, createConnection } from 'typeorm';
+import { join } from 'path';
+import { createConnection } from 'typeorm';
 
-export const connectToDatabase = async (
-  connectionOptions?: ConnectionOptions
-) => {
+export const connectToDatabase = async () => {
   try {
-    if (connectionOptions) {
-      await createConnection(connectionOptions);
-    } else {
-      await createConnection();
-    }
+    await createConnection({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [join(__dirname + '/../../**/*.ts')],
+      synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    });
     // eslint-disable-next-line no-console
     console.log('Connection to DB established.');
   } catch (error) {
